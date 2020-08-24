45a0d3236db37d2212b5a8f4006c842934183ad9
grateful-closure-7122 <alex@artofalexwaller.com>
2020-08-24 02:38:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,6 +29,10 @@ def position_taken?(board,index)
   !(board[index].nil? || board[index]==" ")
 end
 
+def valid_move?(board,index)
+  index.between?(0,8) && !position_taken?(board,index)
+end
+
 def turn_count(board)
   turn=0
   board.each do |index|
