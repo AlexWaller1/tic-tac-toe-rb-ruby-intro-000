986e05054f1db57a88b3da02b54e444e34682865
grateful-closure-7122 <alex@artofalexwaller.com>
2020-08-24 01:59:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,4 +54,31 @@ def turn(board)
   user_input=gets.chomp
   index=input_to_index(user_input)
   if valid_move?(board,index)
-    player_
+    player_turn=current_player(board)
+    move(board,index,player_turn)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each {|winning_trio|
+    index_0=winning_trio[0]
+    index_1=winning_trio[1]
+    index_2=winning_trio[2]
+
+    position_1=board[index_0]
+    position_2=board[index_1]
+    position_3=board[index_2]
+
+    if position_1=="X" && position_2=="X" && position_3=="X"
+      return winning_trio
+    elsif position_1=="O" && position_2=="O" && position_3=="O"
+      return winning_trio
+    end
+  }
+  return false
+end
+
+def full?(board)
